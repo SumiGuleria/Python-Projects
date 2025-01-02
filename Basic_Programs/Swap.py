4f52d55cd6bed0a9ff73078d2b41864b07071c8a
Your Name <sushmitguleria@gmail.com>
2025-01-01 21:48:12 -0500

    changed the folder name and added a few more basic programs

---
+++ Basic_Programs/Swap.py
@@ -0,0 +1,5 @@
+# Swap two variable without using third variable
+a = 10
+b = 20
+a, b = b, a
+print(a, b)
---
+++ Basic_Programs/string_manipulation.py
@@ -0,0 +1,5 @@
+## String Manipulation: Create a string variable. Find its length, convert it to uppercase, and print the first three characters.
+string_var = input("Enter the string here: ")
+print(len(string_var))
+print(string_var.upper())
+print(string_var[:4])
