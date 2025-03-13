5c596c2b25889d4eec873767cdbfafee48f09a22
Your Name <sushmitguleria@gmail.com>
2025-03-13 19:21:39 -0400

    Day 1 small program

--- Small_projects/Day1_bandgenerator.py
+++ Small_projects/Day1_bandgenerator.py
@@ -1,3 +1,6 @@
 # A program to come up with a band name.
-print("Hello World \nHello world")
+print("Welcome to the band generator")
+city = input("What's the name of the city you grew up in?")
+pet = input("What is your pet's name?")
+print("The name of your band could be: " + city + " "+ pet )
 
